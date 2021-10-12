e15ff5367ee3c47661bf9c1e7973dbd8451bcb12
Leo <335209779@qq.com>
2021-10-12 15:01:32 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fiscoSharespot/routes"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	engine := gin.Default()
+	engine.GET("/ping", routes.PingHandler)
+	engine.GET("/blockNumber", routes.BlockNumber)
+
+	if err := engine.Run(":8080"); err != nil {
+		fmt.Printf("Server failed to start, err: %v", err)
+	}
+
+	//fmt.Println(number)
+	//
+	//if err != nil {
+	//	return
+	//}
+	//input := "Store deployment 1.0"
+	//address, tx, instance, err := store.DeployStore(conn.GetTransactOpts(), conn, input)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("contract address: ", address.Hex()) // the address should be saved, will use in next example
+	//fmt.Println("transaction hash: ", tx.Hash().Hex())
+	//
+	////load the contract
+	////contractAddress := common.HexToAddress("contract address in hex String")
+	////instance, err := store.NewStore(contractAddress, conn)
+	////if err != nil {
+	////	log.Fatal(err)
+	////}
+	//
+	//fmt.Println("================================")
+	//storeSession := &store.StoreSession{Contract: instance, CallOpts: *conn.GetCallOpts(), TransactOpts: *conn.GetTransactOpts()}
+	//
+	//version, err := storeSession.Version()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println("version :", version) // "Store deployment 1.0"
+	//
+	//// contract write interface demo
+	//fmt.Println("================================")
+	//key := [32]byte{}
+	//value := [32]byte{}
+	//copy(key[:], []byte("foo"))
+	//copy(value[:], []byte("bar"))
+	//
+	//tx, receipt, err := storeSession.SetItem(key, value)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Printf("tx sent: %s\n", tx.Hash().Hex())
+	//fmt.Printf("transaction hash of receipt: %s\n", receipt.GetTransactionHash())
+	//
+	//// read the result
+	//result, err := storeSession.Items(key)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println("get item: " + string(result[:])) // "bar"
+}
---
+++ utils/utils.go
@@ -0,0 +1,36 @@
+package utils
+
+type Resp struct {
+	Recode string `json:"recode"`
+	Msg string `json:"msg"`
+	// valid data
+	Data interface{} `json:"data"`
+}
+
+// scheduled some predictable err recodes
+const (
+	RecodeOk        = "0"
+	RecodeDBErr     = "4001"
+	RecodeLoginErr  = "4002"
+	RecodeParamErr  = "4003"
+	RecodeSysErr     = "4004"
+	RecodeFiscoErr   = "4105"
+	RecodeUnknownErr = "4106"
+)
+
+var recodeText = map[string]string{
+	RecodeOk:         "Success",
+	RecodeDBErr:      "Database operation error",
+	RecodeLoginErr:   "User login error",
+	RecodeParamErr:   "Parameter error",
+	RecodeSysErr:     "System error",
+	RecodeFiscoErr:   "Error interacting with Fisco",
+	RecodeUnknownErr: "Unknown error",
+}
+
+func RecodeText(code string) string {
+	if str, ok := recodeText[code]; ok {
+		return str
+	}
+	return recodeText[RecodeUnknownErr]
+}
